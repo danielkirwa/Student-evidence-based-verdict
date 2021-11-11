de12919d49dcf0183f9b0c6bd1b80a8647572acb
danielkirwa <kirwadan03@gmail.com>
2021-11-11 12:49:00 +0300

    first commit

---
+++ src/studentverdict/StudentVerdict.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentverdict;
+
+/**
+ *
+ * @author owner
+ */
+public class StudentVerdict {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       new Login().setVisible(true);
+    }
+    
+}
